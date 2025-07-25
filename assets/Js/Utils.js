642010aca54e08738d0029b4b539e609adf81449
Your Name <kasra.oliver.quin@gmail.com>
2025-07-25 20:50:03 +0330

    v-4.0.1

--- assets/Js/Utils.js
+++ assets/Js/Utils.js
@@ -1,12 +1,2 @@
 // js/utils.js
 
-export function lockScroll() {
-    document.documentElement.style.overflow = 'hidden';
-    document.body.style.overflow = 'hidden';
-}
-
-export function unlockScroll() {
-    document.documentElement.style.overflow = '';
-    document.body.style.overflow = '';
-}
-
